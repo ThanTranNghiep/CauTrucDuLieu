7f7e11b819ce0a0b82996604ad94b24799ebd4c0
ThanTranNghiep <thantrannghiep@gmail.com>
2021-09-14 16:40:46 +0700

    14/9/2021

--- bt1.cpp
+++ bt1.cpp
@@ -30,6 +30,35 @@ void insertFist(list &l,node* x){
 	l.head=x;
 	return;
 }
+void DeleteHead(list &l){
+	if(l.head==l.tail){
+		l.head=l.tail=NULL;
+		return;
+	}
+	node* i=l.head->next;
+	free(l.head);
+	l.head=i;
+	return;
+}
+void DelAfter(list &l,int value){
+	node *i=l.head;
+	while (i->Data != value )
+	{
+		i=i->next;
+	}
+	node *a=i->next;
+	i->next=i->next->next;
+	free(a);
+}
+void addAfter(list &l,int value,node*x){
+	node *i=l.head;
+	while (i->Data != value )
+	{
+		i=i->next;
+	}
+	x->next=i->next;
+	i->next=x;
+}
 void case1(list &l){
 	int temp;
 	printf("\nNhap: ");
@@ -44,6 +73,21 @@ void case2(list l){
 			printf("%4d",i->Data);
 			i=i->next;
 		}
+}
+void case4(list &l){
+	int value;
+	printf("\nNhap value: ");
+	scanf("%d",&value);
+	DelAfter(l,value);
+}
+void case5(list &l){
+	int value,pos;
+	printf("Insert  : ");
+	scanf("%d",&value);
+	printf(" pos : ");
+	scanf("%d",&pos);
+	node *x = CreateNode(value);
+	addAfter(l,pos,x);
 }
 int main(){
 	list l;
@@ -63,6 +107,15 @@ int main(){
 			break;
 		case 2:
 			case2(l);
+			break;
+		case 3:
+			DeleteHead(l);
+			break;
+		case 4:
+			case4(l);
+			break;
+		case 5:
+			case5(l);
 			break;
 		default:
 				exit(0);
