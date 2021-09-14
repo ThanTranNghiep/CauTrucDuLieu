e66d8fa931849c3b51ea67bc42d39923d0660602
ThanTranNghiep <thantrannghiep@gmail.com>
2021-09-14 20:05:17 +0700

    14/09/2021

--- bt1.cpp
+++ bt1.cpp
@@ -89,14 +89,49 @@ void case5(list &l){
 	node *x = CreateNode(value);
 	addAfter(l,pos,x);
 }
+bool Find(list l,int pos){
+	node*i=l.head;
+	while (i!=NULL)
+	{
+		if(i->Data == pos)
+			return true;
+		i=i->next;
+	}
+	return false;
+}
+void Sort(list &l){
+	node*i ;
+	node*j ;
+	for(i=l.head;i->next!=NULL;i=i->next)
+	{
+		for(j=i->next;j!=NULL;j=j->next)
+		{
+			if(i->Data > j->Data)
+			{
+				node*a;
+				a->Data=i->Data;
+				i->Data=j->Data;
+				j->Data=a->Data;
+			}
+		}
+	}
+}
 int main(){
 	list l;
+	int chon;
 	initList(l);
-	printf("\n0.Thoat");;
+	do{
+		system("cls");
+	printf("\n0.Thoat");
 	printf("\n1.Them phan tu");
 	printf("\n2.xuat danh sach");
-	int chon;
-	do{
+	printf("\n3.Xoa phan tu dau danh sach");
+	printf("\n4.Xoa phan tu phia sau p");
+	printf("\n5.Them phan tu sau p");
+	printf("\n6.Tim kiem");
+	printf("\n7.Sap xep tang dan");
+	
+	
 		printf("\nChon: ");
 		scanf("%d",&chon);
 	
@@ -107,6 +142,7 @@ int main(){
 			break;
 		case 2:
 			case2(l);
+			system("pause");
 			break;
 		case 3:
 			DeleteHead(l);
@@ -117,9 +153,27 @@ int main(){
 		case 5:
 			case5(l);
 			break;
+		case 6:
+			int pos;
+			printf("Nhap so can tim: ");
+			scanf("%d",&pos);
+			if(Find(l,pos)==true)
+			{
+					printf("\nTim thay !");
+			}
+			else
+			{
+				printf("\nKhong tim thay !");
+			}
+			system("pause");
+			break;
+		case 7:
+			Sort(l);
+			system("pause");
+			break;
 		default:
 				exit(0);
-		}
+		}	
 	}while(chon !=0);
 	return 0;
 }
